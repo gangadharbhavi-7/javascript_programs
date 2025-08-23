51412bb4c8bbdf90a0512b1401fff856b1ffcf9d
Gangadhar B <gangadharbavi312004@gmail.com>
2025-08-23 21:23:49 +0530

    Create program_5.js

---
+++ practice_questions_code_with_harry/chapter-1/program_5.js
@@ -0,0 +1,7 @@
+let dictionary = {
+  "hello": "A greeting or expression of goodwill.",
+  "world": "The earth or globe we live on.",
+  "javascript": "A high-level, dynamic programming language.", 
+  "node": " A Javascript runtime built on chrome's v8 engine."
+};
+console.log(dictionary["hello"]);
