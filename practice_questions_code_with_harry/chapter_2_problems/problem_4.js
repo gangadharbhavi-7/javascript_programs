53679cd064fd93b565b70b0131e4f0c0ff43d760
Gangadhar B <gangadharbavi312004@gmail.com>
2025-09-01 20:59:02 +0530

    Create problem_4.js

---
+++ practice_questions_code_with_harry/chapter_2_problems/problem_4.js
@@ -0,0 +1,8 @@
+let number = 21;
+console.log("The number is : " + number);
+if(number%2==0 || number%3==0){
+  console.log("The number is divisible by either 2 or 3 ");
+}
+else{
+  console.log("The number is not divisible by 2 or 3");
+}
