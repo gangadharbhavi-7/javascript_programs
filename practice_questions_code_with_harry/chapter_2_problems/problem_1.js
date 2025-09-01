b0353cc864b84d0ecef877560fca8da259f32cf7
Gangadhar B <gangadharbavi312004@gmail.com>
2025-09-01 20:27:36 +0530

    Create problem_1.js

---
+++ practice_questions_code_with_harry/chapter_2_problems/problem_1.js
@@ -0,0 +1,9 @@
+let age =23;
+console.log("your age is"+ age);
+if(age>10 && age<20){
+  console.log("your age is between 10 and 20");
+}
+else{
+  console.log("your age is not between 10 and 20");
+}
+
